203d0fc28827668d57879100a12d4fcc5b50715e
Zidri <99145624+Zidri@users.noreply.github.com>
2022-03-16 11:33:31 -0600

    Add files via upload

---
+++ src/assignment_combobox/Assignment_Combobox.java
@@ -0,0 +1,32 @@
+/**
+ * Joanna Hannigan 
+ * ComboBox Assignment
+ * Description: A program that does the following
+ * 1. Uses separate text boxes for customer first and last name
+ * 2. Uses a combobox for either small - $3, medium - $5, or large - $7
+ * 3. Uses a listbox for toppings (each an additional $1): 
+ *          Cheese, Beef, Pepperoni, Sausage, Italian Sausage, Ham, Bacon
+ *          Multiple items can be selected
+ * 4. Uses a listbox for extras (each an additional $0.50):
+ *          Onions, Pineapple, Mushrooms, Extra Cheese, Peppers
+ *          Multiple items can be selected
+ * 5. Uses a combobox for Pick-up or Delivery (Delivery adds $5)
+ * 6. List the order in a textarea with specifics that updates in real time
+ * Updated: 2/15/2022
+ */
+package assignment_combobox;
+
+/**
+ *
+ * @author joannaahannigan
+ */
+public class Assignment_Combobox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
